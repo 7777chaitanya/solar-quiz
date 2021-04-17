c4532872bea688de3b704d0f0cdd08aeb85c9c9c
Chaitanya J <>
2021-04-17 12:32:37 +0000

    update: index.js questions

--- index.js
+++ index.js
@@ -53,13 +53,13 @@ if(score>=3){
 
   var score1=0;
 
-  var q6 = "Which of the following part of the Sun is visible to humans?\n1) Photosphere\n2) Corona\n3) Chromospheres\n4) Core\n"
+  var q6 = "What is the Sun mainly made from?\n1) Gas\n2) Molten Iron\n3) Liquid Lava\n4) Rock\n"
 
-  var q7 = "Which of the following part of the Sun is visible at the time of the eclipse?\n1) Photosphere\n2) Corona\n3) Chromosphere\n4) Core\n"
+  var q7 = "In which planet is the largest volcano in the Solar System located?\n1) Earth\n2) Venus\n3) Mars\n4) Jupiter\n"
 
-  var q8 = "Which is the deepest point from the sea level on the Earth?\n1) North Channel\n2) Pacific Ocean\n3) Mariana Trench\n4) Red Sea\n"
+  var q8 = "Which planet do the moons Oberon and Titania belong to?\n1 Earth\n2) Jupiter\n3) Venus\n4) Uranus\n"
 
-  var questions1 = [{question:q6,answer:1},{question:q7,answer:2},{question:q8,answer:3}];
+  var questions1 = [{question:q6,answer:1},{question:q7,answer:3},{question:q8,answer:4}];
 
   for(var i=0;i<questions1.length;i++){
   console.log(chalk.green.bold.bgBlue("----------------------QUESTION "+(i+1)+"----------------------"));
